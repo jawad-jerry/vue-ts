0f24b4d71824446d32600448f0e539380eb82e9a
jawadAHMA <121359698+jawadAHMA@users.noreply.github.com>
2023-02-18 11:50:43 -0800

    fixed

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import {ref} from 'vue';
 const counter = ref(0)
 const count = () =>{
@@ -23,30 +22,6 @@ const count = () =>{
     </div>
 </main>
 
-=======
-import { ref } from "vue";
-const counter = ref(0);
-const count = () => {
-  if (counter.value <= 0) {
-    return;
-  } else {
-    counter.value--;
-  }
-};
-</script>
-<template>
-  <main>
-    <div>
-      <h3>the current count is --</h3>
-      <h1>{{ counter }}</h1>
-      <button @click="count">-</button>
-      <button @click="counter++" style="display: inline-block">+</button>
-      <br />
-      <br />
-      <button class="btn" v-if="counter" @click="counter = 0">reset</button>
-    </div>
-  </main>
->>>>>>> origin/main
 </template>
 
 <style scoped>

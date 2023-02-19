94e4aae7ecbfc4a4dfdb5710784716ac7c29e9ac
Waheed Sindhani <waheed.eliccs@gmail.com>
2023-02-19 23:18:00 +0500

    router fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
@@ -18,24 +18,24 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path:'/todos',
-      name: 'todos.index',
-      component: () => import('../views/todos/index.vue')
+      path: "/todos",
+      name: "todos.index",
+      component: () => import("../views/todos/index.vue"),
     },
     {
-      path:'/todos/create',
-      name: 'todos.create',
-      component: () => import('../views/todos/create.vue')
+      path: "/todos/create",
+      name: "todos.create",
+      component: () => import("../views/todos/create.vue"),
     },
     {
-      path:'/todos/:id/detail',
-      name: 'todos.detail',
-      component: () => import('../views/todos/detail.vue')
+      path: "/todos/:id/detail",
+      name: "todos.detail",
+      component: () => import("../views/todos/detail.vue"),
     },
     {
-      path:'/todos/:id/update',
-      name: 'todos.update',
-      component: () => import('../views/todos/update.vue')
+      path: "/todos/:id/update",
+      name: "todos.update",
+      component: () => import("../views/todos/update.vue"),
     },
   ],
 });

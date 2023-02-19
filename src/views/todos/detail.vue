55b70df36a7f521330f38b0fab9df679a0c852ff
Waheed Sindhani <waheed.eliccs@gmail.com>
2023-02-19 23:33:10 +0500

    detail page bug fixed

--- src/views/todos/detail.vue
+++ src/views/todos/detail.vue
@@ -1,21 +1,22 @@
 <script lang="ts" setup>
 import { useRoute, type RouteParamValueRaw } from "vue-router";
-import type { Ref } from 'vue'
-import { reactive } from "vue";
-interface Todo{
-  id?: Number,
-  title?: String,
-  body?: String,
-  userId?: Number
+import type { Ref } from "vue";
+import { ref, nextTick } from "vue";
+interface Todo {
+  id?: Number;
+  title?: String;
+  body?: String;
+  userId?: Number;
 }
 const route = useRoute();
-let todoDetail:Todo = reactive({});
+let todoDetail: Ref<Todo> = ref({});
 const url = `https://jsonplaceholder.typicode.com/posts/${route.params.id}`;
 
 fetch(url)
   .then((data) => data.json())
-  .then((data) => {
-    todoDetail = data;
+  .then(async (data) => {
+    // await nextTick();
+    todoDetail.value = data;
   });
 </script>
 <template>
